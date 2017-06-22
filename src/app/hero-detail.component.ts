491148f3ac06ad8747ad06a5083c657903ff4e18
jianye.tang <jianye.tang@aorise.org>
2017-06-22 14:51:13 +0800

    Multiple Components

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by tangjy on 2017/6/22.
+ */
+import { Component, Input } from '@angular/core';
+
+import { Hero } from './hero';
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class HeroDetailComponent {
+  @Input() hero: Hero;
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by tangjy on 2017/6/22.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
